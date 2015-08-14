4784d2feb36508fafbb0c17a0700fd4089d6d98a
lujxio <jcdiaz92@gmail.com>
2015-08-14 10:32:41 -0700

    write all test pass all test

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -5,7 +5,7 @@
 		{
 			$count = 0;
 			$pos = -1;
-			while($pos = strpos($stringToMatch, $wordToMatch, $pos + 1) !== false) {
+			while(($pos = strpos(strtolower($stringToMatch), strtolower($wordToMatch), $pos + 1)) !== false) {
 				$count += 1;
 			}
 			return $count;
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -3,7 +3,7 @@
 	
 	class RepeatCounterTest extends PHPUnit_Framework_TestCase
 	{
-		function testOneWord(){	
+		function testOne(){	
 			//Arrange
 			$test = new RepeatCounter;
 			$inputWord = "An";
@@ -16,6 +16,61 @@
 			$this->assertEquals(1, $result);
 			
 		}
+		
+		function testTwo(){	
+			//Arrange
+			$test = new RepeatCounter;
+			$inputWord = "no";
+			$inputString = "no way";
 			
+			//Act
+			$result = $test->countRepeats($inputWord, $inputString);
+			
+			//Assert
+			$this->assertEquals(1, $result);
+			
+		}
+		
+		function testThree(){	
+			//Arrange
+			$test = new RepeatCounter;
+			$inputWord = "bat";
+			$inputString = "The bat swung";
+			
+			//Act
+			$result = $test->countRepeats($inputWord, $inputString);
+			
+			//Assert
+			$this->assertEquals(1, $result);
+			
+		}
+		
+		function testFour(){	
+			//Arrange
+			$test = new RepeatCounter;
+			$inputWord = "no";
+			$inputString = "No way man, no way";
+			
+			//Act
+			$result = $test->countRepeats($inputWord, $inputString);
+			
+			//Assert
+			$this->assertEquals(2, $result);
+			
+		}
+		
+		function testFive(){	
+			//Arrange
+			$test = new RepeatCounter;
+			$inputWord = "hello";
+			$inputString = "hello john hello gabriel hello dog hello cat hello evil monster hello";
+			
+			//Act
+			$result = $test->countRepeats($inputWord, $inputString);
+			
+			//Assert
+			$this->assertEquals(6, $result);
+			
+		}
 	}
 ?>
