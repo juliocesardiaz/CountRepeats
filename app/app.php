3573962b7b72171231e1b0343acb54d5ad53d60c
lujxio <jcdiaz92@gmail.com>
2015-08-14 10:43:07 -0700

    write app and index

---
+++ app/app.php
@@ -0,0 +1,20 @@
+<?php 
+	require_once __DIR__."/../vendor/autoload.php";
+	require_once __DIR__."/../src/RepeatCounter.php";
+	
+	$app = new Silex\Application();
+	$app->register(new Silex\Provider\TwigServiceProvider(), array(
+		'twig.path' => __DIR__.'/../views'
+	));
+	
+	$app->get("/", function() use ($app) {
+		return $app['twig']->render('home.html.twig');
+	});
+	$app->get("/result", function() use ($app) {
+		$wordCount = new RepeatCounter;
+		$result = $wordCount->countRepeats($_GET['word'], $_GET['wordString']);
+		return $app['twig']->render('result.html.twig', array('result' => $result));
+	});
+	
+	return $app;
+?>
---
+++ web/index.php
@@ -0,0 +1,4 @@
+<?php 
+	$website = require_once __DIR__.'/../app.php';
+	$website->run();
+?>
