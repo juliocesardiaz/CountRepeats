1a0aefcad58cb39e2ae17947f100d96522e778f8
lujxio <jcdiaz92@gmail.com>
2015-08-14 10:57:30 -0700

    finished web pages

--- app/app.php
+++ app/app.php
@@ -3,6 +3,7 @@
 	require_once __DIR__."/../src/RepeatCounter.php";
 	
 	$app = new Silex\Application();
+	$app['debug'] = true;
 	$app->register(new Silex\Provider\TwigServiceProvider(), array(
 		'twig.path' => __DIR__.'/../views'
 	));
