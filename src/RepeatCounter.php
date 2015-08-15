570487f8265197d040f0f171700df0e899048cf1
Julio <juliocesardiaz@users.noreply.github.com>
2015-08-14 17:52:08 -0700

    Update RepeatCounter.php

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,14 +4,13 @@
 		/*
 			Function: countRepeats
 			Arguments: $wordToMatch: string of the word you want to count
-					   $stringToMatch: string to count the amount of occurences of $wordToMatch
+			           $stringToMatch: string to count the amount of occurences of $wordToMatch
 			Returns: int containing amount of times wordToMatch occurs in $stringToMatch
 		*/
 		function countRepeats($wordToMatch, $stringToMatch)
 		{
 			$count =  preg_match_all("/\b" . $wordToMatch . "\b/", $stringToMatch);
 			return $count;
-		}
 		}
 	}
 ?>
